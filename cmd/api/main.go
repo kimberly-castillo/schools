e29ce444408227b2ce9204a2ddfdcc61379cd904
Kimberly Castillo <2021154911@ub.edu.bz>
2022-08-22 13:32:32 -0600

    First Project Structure and Test with main function

---
+++ cmd/api/main.go
@@ -0,0 +1,14 @@
+//make sure you write the name of the file that you are coding in. This one is: main.go
+// Filename: where the file is in relation to the name
+//have to create a main package which contains the main function
+// Filename: cmd/api/main.go
+
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("This is a test.")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module schools.kimberlycastillo.net
+
+go 1.19
